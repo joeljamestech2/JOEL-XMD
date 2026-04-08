b3da82d6f5096d42e3cdf0d584fea8edda9dce26
~joeljamestech <162905644+joeljamestech2@users.noreply.github.com>
2026-04-08 12:05:45 +0300

    Delete framework directory

--- framework/james.js
+++
@@ -1,8 +0,0 @@
-} catch (error) {
-    console.error('Error in videoHandler:', error.message || error);
-    await sock.sendMessage(m.from, {
-      text: "```An unexpected error occurred! Both primary and fallback failed.```",
-    }, { quoted: m });
-    if (typeof m.React === 'function') await m.React('❌');
-  }
-}
